57ec96350a41d80ca4b6e00d37f702968f130440
özgür kavak <ozgurkvk15@gmail.com>
2026-09-07 17:18:58 +0300

    İkinci dereceden matematiksel ifade hesaplama örneği eklendi

---
+++ Soru-05/Soru05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Soru05 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c,x,sonuc;
+
+        System.out.print("a,b,c,x, degerlerini giriniz: ");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        x = scanner.nextInt();  
+
+        sonuc = a*x*x + b*x + c;
+
+        System.out.println("Sonuç: " + sonuc);
+        scanner.close();
+
+    }
+    
+}
